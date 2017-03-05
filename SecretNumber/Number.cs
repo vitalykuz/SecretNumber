42d08552380362302e11025b4559392a70855917
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-05 15:25:11 +1100

    Add Number class and GetRandomNumberInRange method

--- SecretNumber/Number.cs
+++ SecretNumber/Number.cs
@@ -1,7 +1,15 @@
-﻿namespace SecretNumber
+﻿using System;
+
+namespace SecretNumber
 {
     public class Number
     {
-        
+
+        public int GetRandomNumberInRange(int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomNumber = random.Next(minValue, maxValue);
+            return randomNumber;
+        }               
     }
 }
