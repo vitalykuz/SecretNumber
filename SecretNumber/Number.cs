128e1004f08ff901c65f0800bc79add7c01c7cd9
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-03-05 16:37:59 +1100

    Added Number class and several methids to deal with user inout

--- SecretNumber/Number.cs
+++ SecretNumber/Number.cs
@@ -10,6 +10,37 @@ namespace SecretNumber
             var random = new Random();
             var randomNumber = random.Next(minValue, maxValue);
             return randomNumber;
-        }               
+        }
+
+
+        public int GetRandomNumberWithMessage(string message)
+        {
+            var convertedValue = -1;
+            while (convertedValue == -1)
+            {
+                Console.WriteLine(message);
+                var value = Console.ReadLine();
+                convertedValue = ConvertStringToInt(value);
+            }
+
+            return convertedValue;
+        }
+
+        public int ConvertStringToInt(string stringToConvert)
+        {
+            var convertedInt = 0;
+
+            if (int.TryParse(stringToConvert, out convertedInt))
+            {
+                // you know that the parsing attempt was successful
+                return convertedInt;
+            }
+            return -1;
+        }
+
+        public void CheckUserGuess(int userGuess, int secretNumber)
+        {
+            
+        }
     }
 }
--- SecretNumber/Program.cs
+++ SecretNumber/Program.cs
@@ -1,4 +1,38 @@
-﻿using System;
+﻿/*
+ * Lab 1 - Guess a number
+
+    Requirements
+
+    You are required to produce a working console program that generates a random "secret" number 
+    between 1 and 100 and asks the user to guess it. The user will enter a number and the program 
+    will take one of the following actions:
+
+    If the number entered by the user is greater or less than the secret number, the program will display 
+    an informative message and allow the user to continue trying.
+
+    If the number entered by the user equals the secret number, the program will display 
+    a success message showing how many tries were needed to guess the number.
+    
+    Additional functionality
+
+    You can optionally add the following functionality to the program:
+
+    The program will save the last 5 scores (number of tries) and display them on demand.
+    The program will allow the user to select the range of numbers to consider when generating the random 
+    secret number.
+    The program will measure the time that the user takes to guess the secret number, and display it 
+    along the score.
+   
+    Hints
+
+    Use the class System.Random to generate the secret number.
+    Use System.DateTime to measure time.
+    You might use List<int> or Queue<int> in System.Collections.Generic to store a list of past values.
+    Feel free to discuss on the UTS Online Discussion Boards if you get stuck.
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +44,20 @@ namespace SecretNumber
     {
         static void Main(string[] args)
         {
-        }
+            var numberClass = new Number();
+
+            var minValue = numberClass.GetRandomNumberWithMessage("Please provide a minimum value to generate a random number: ");
+            var maxValue = numberClass.GetRandomNumberWithMessage("Please provide a maximum value to generate a random number: ");
+
+            var secretNumber = numberClass.GetRandomNumberInRange(minValue, maxValue);
+            Console.WriteLine("Secret Number is : {0}",secretNumber);
+
+            var userGuess = numberClass.GetRandomNumberWithMessage("Try to guess the secret number: ");
+
+            numberClass.CheckUserGuess(userGuess, secretNumber);
+
+
+            Console.ReadLine();
+        } 
     }
 }
